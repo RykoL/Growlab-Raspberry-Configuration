881f0fa58c5b9222e61202b90a7ca50a48c5d6a9
Rico Lang <rico.lang@thoughtworks.com>
2023-03-20 09:16:59 +0100

    Remove unused code

--- infrastructure/service-account.tf
+++ infrastructure/service-account.tf
@@ -3,20 +3,6 @@ resource "google_service_account" "image_upload" {
   display_name = "Image upload service account"
 }
 
-# data "google_iam_policy" "admin" {
-#   binding {
-#     role = "roles/storage.admin"
-#     members = [
-#       "serviceAccount:${google_service_account.image_upload.email}"
-#     ]
-#   }
-# }
-
-# resource "google_storage_bucket_iam_policy" "policy" {
-#   bucket = google_storage_bucket.images.name
-#   policy_data = data.google_iam_policy.admin.policy_data
-# }
-
 resource "google_storage_bucket_iam_binding" "binding" {
   bucket = google_storage_bucket.images.name
   role = "roles/storage.admin"
